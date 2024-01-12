d40a689bbf7ae9d18996dc8245dc84b8434fef43
Shane Poppleton <shane@bjja.com.au>
2024-01-12 14:08:51 +1000

    Removed ClickSendService

--- src/ClickSendServiceProvider.php
+++ src/ClickSendServiceProvider.php
@@ -24,19 +24,10 @@ class ClickSendServiceProvider extends ServiceProvider
             return new ClickSendConfig($this->app['config']['clicksend-notification-channel']);
         });
 
-        $this->app->singleton(ClickSendService::class, function (Application $app) {
-            $config = $app->make(ClickSendConfig::class);
 
-            if (!$config->configurationValid()) {
-                throw InvalidConfigException::missingConfig();
-            }
-
-            return new ClickSendService($config->getUsername(), $config->getPassword());
-        });
 
         $this->app->singleton(ClickSend::class, function(Application $app) {
            return new ClickSend(
-               $app->make(ClickSendService::class),
                $app->make(ClickSendConfig::class)
            );
         });
@@ -53,7 +44,6 @@ class ClickSendServiceProvider extends ServiceProvider
     {
         return [
             ClickSendConfig::class,
-            ClickSendService::class,
             ClickSendChannel::class
         ];
     }
